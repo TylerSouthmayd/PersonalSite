47edc73a058a223f5bb7c1cfedcfbfa2e7ab031c
tjs11010 <tyler.southmayd@uconn.edu>
2015-08-07 00:07:47 -0400

    intro page styling and functionality

--- js/controller/homeController.js
+++ js/controller/homeController.js
@@ -3,13 +3,43 @@
  */
 mainApp.controller('HomeController', HomeController);
 
-function HomeController($scope)
+function HomeController($scope, $timeout)
 {
-    $scope.showNav = true;
+    $scope.showNav = false;
 
     $scope.toggleNav = function()
     {
         $scope.showNav = !$scope.showNav;
     };
+    $scope.introText = '';
+    var introString = "Hello, I'm Tyler Southmayd. \nWelcome to my personal website......."
+    addLineWithCharDelay(introString, 75);
+
+    //@Param - String line to add to console output
+    //       - int delay (ms)
+    function addLineWithCharDelay(line, delay)
+    {
+        for(var i = 0; i < line.length; i++)
+        {
+            (function(index)
+            {
+                $timeout(function()
+                {
+                    $scope.introText += line.charAt(index);
+                    $scope.$apply();
+                }, delay*i);
+            })(i);
+        }
+    }
+
+    function addLineNoDelay(line)
+    {
+        $scope.introText += line;
+    }
+
+    function newTerminalLine()
+    {
+        addLineNoDelay('\n');
+    }
 
 }
